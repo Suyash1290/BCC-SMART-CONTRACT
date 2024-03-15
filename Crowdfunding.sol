a3fd71b3ff5835e26f2b2e9583d7532e4f489a0d
Suyash1290 <129292979+Suyash1290@users.noreply.github.com>
2024-03-15 20:11:53 +0530

    Update Crowdfunding.sol

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -1,6 +1,5 @@
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
